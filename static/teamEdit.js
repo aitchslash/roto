fa650a5c0c2158725e31ba9c47b485a9936a74cf
Benjamin Field <benjamin.field@gmail.com>
2015-04-16 12:21:29 -0400

    start work on scaling AB

---
+++ static/teamEdit.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	scale_ab();
+})
+
+function scale_ab () {
+	if ($('#team_form .AB :enabled').length == 0) {
+		console.log('HERERERERERE');
+		$('#team_form .AB input').each(function () {
+			$(this).removeAttr('disabled');
+			// console.log($(this).val());
+			// console.log(this)
+		})
+	};
+}
