74147c60473155e110c523677c22652c903148d5
Benjamin Field <benjamin.field@gmail.com>
2015-04-22 12:26:09 -0400

    fix team_edit inputs (G, AB) progress on form handling

--- project.py
+++ project.py
@@ -59,8 +59,35 @@ def editTeam(teamID):
         # joeyH = request.form['bautijo02H']
         # print joeyH
         ids = form_data.getlist('lahmanID')
+
         print ids
-        # render_template('team_edit.html', team_data=team_batting_data)
+        # loop through players, grab data, commit new objs
+        for lahmanID in ids:
+            # entry obj
+            batter_obj = session.query(Batting).filter(Batting.lahmanID == lahmanID, Batting.yearID == 2015).one()
+            # print "ID: " + lahmanID + "  HR: " + request.form[lahmanID + "AB"]
+            # print "  G: " + request.form[lahmanID + "G"]
+            batter_obj.G = request.form[lahmanID + "G"]
+            # print batter_obj.G
+
+            batter_obj.AB = request.form[lahmanID + "AB"]
+            batter_obj.H = request.form[lahmanID + "H"]
+            batter_obj.CS = request.form[lahmanID + "CS"]
+            batter_obj.IBB = request.form[lahmanID + "IBB"]
+            batter_obj.R = request.form[lahmanID + "R"]
+            batter_obj.doubles = request.form[lahmanID + "2B"]
+            batter_obj.triples = request.form[lahmanID + "3B"]
+            batter_obj.HR = request.form[lahmanID + "HR"]
+            batter_obj.RBI = request.form[lahmanID + "RBI"]
+            batter_obj.SB = request.form[lahmanID + "SB"]
+            batter_obj.BB = request.form[lahmanID + "BB"]
+            batter_obj.SO = request.form[lahmanID + "SO"]
+            batter_obj.HBP = request.form[lahmanID + "HBP"]
+            batter_obj.GIDP = request.form[lahmanID + "GIDP"]
+            batter_obj.SH = request.form[lahmanID + "SH"]
+            batter_obj.SF = request.form[lahmanID + "SF"]
+
+            # render_template('team_edit.html', team_data=team_batting_data)
         return redirect(url_for('teamPage', teamID=teamID))
     else:
         return render_template('team_edit.html', team_data=team_batting_data)
