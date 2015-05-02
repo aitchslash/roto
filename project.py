df71263621bfa1ea72bb11ebb7b11da2210a2c2c
Benjamin Field <benjamin.field@gmail.com>
2015-05-02 14:47:10 -0400

    fix up login w/ g+

--- project.py
+++ project.py
@@ -345,16 +345,17 @@ def gconnect():
         user_id = createUser(login_session)
         login_session['user_id'] = user_id
 
-        output = ''
-        output += '<h1>Welcome, '
-        output += login_session['username']
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
 
-        output += '!</h1>'
-        output += '<img src="'
-        output += login_session['picture']
-        output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-        flash("you are now logged in as %s" % login_session['username'])
-        return output
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print output
+    return output
 
     '''
     output = ''
