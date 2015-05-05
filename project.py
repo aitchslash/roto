2abdfb95aec0b0bb332ddac4467f419cce7b7d4e
Benjamin Field <benjamin.field@gmail.com>
2015-05-05 13:57:36 -0400

    try to get long lived token

--- project.py
+++ project.py
@@ -418,10 +418,19 @@ def fbconnect():
     # Exchange client token for long-lived server-side token
     # # GET /oauth/access_token?grant_type=fb_exchange_token&client_id={app-id}&client_secret={app-secret}&fb_exchange_token={short-lived-token}
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    print "app_id"
+    print app_id
     app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    print "app_secret"
+    print app_secret
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
+    print "url"
+    print url
     h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
+    # result = h.request(url, 'GET')[1]
+    result = h.request(url, 'GET')
+    print "result"
+    print result
 
     # Use token to get user info from API
     # userinfo_url = "https://graph.facebook.com/v2.2/me"
