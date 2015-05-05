ed7a563e4437cebf8947051d8c53180a73baaa3f
Benjamin Field <benjamin.field@gmail.com>
2015-05-05 13:11:45 -0400

    tinker w/ fb login

--- project.py
+++ project.py
@@ -424,16 +424,17 @@ def fbconnect():
     result = h.request(url, 'GET')[1]
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.2/me"
+    # userinfo_url = "https://graph.facebook.com/v2.2/me"
     # strip expire tag from access token
     token = result.split("&")[0]
 
     url = 'https://graph.facebook.com/v2.2/me?%s' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
+    print "url sent for API access:%s" % url
+    print "API JSON result: %s" % result
     data = json.loads(result)
+    print data
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
