e3e8e8473df3a649be82b760011f81a0c5c41126
Benjamin Field <benjamin.field@gmail.com>
2015-05-11 10:31:53 -0400

    comment out test lines

--- static/teamEdit.js
+++ static/teamEdit.js
@@ -25,10 +25,10 @@ function set_team() {
 	var urlString = window.location.href;
 	var splitter = urlString.indexOf("team/");
 	var teamID = urlString.substring(splitter + 5, splitter + 8);
-	console.log(teamID);
+	// console.log(teamID);
 	var user = urlString.substring(urlString.indexOf("/edit") + 5, urlString.indexOf("/edit") + 7);
-	console.log(user);
-	console.log('/team/' + teamID + user);
+	// console.log(user);
+	// console.log('/team/' + teamID + user);
 	$('#team_sel select').val('/team/' + teamID + user);
 }
 
